4774de1a4ee33001045acbf8e059b3e6f3dc9ab5
Pablo Reinhardt <pabloreinhardt@gmail.com>
2023-08-11 15:02:22 -0300

    [alt] Alteração da main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
-use crossterm::{cursor, event, style};
+use std::{io::{self, Write, stdout}, time::Duration, thread};
+use crossterm::{
+    queue,
+    event::{read, KeyCode},
+    ExecutableCommand, QueueableCommand,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen, DisableLineWrap, enable_raw_mode, disable_raw_mode}, cursor::{self, MoveDown, MoveLeft, MoveRight, MoveUp}, style::{self, Stylize}, event::{Event, KeyEvent, PushKeyboardEnhancementFlags, KeyboardEnhancementFlags, PopKeyboardEnhancementFlags}
+};
 
-fn main() {
+mod structs;
 
+fn main() -> io::Result<()> {
+    let mut a = structs::terminal::Terminal::start()?;
+    a.init()?;
+    a.matar()?;
+    Ok(())
+}
+
+fn print_events() -> io::Result<()>{
+    println!("Começando agora aos eventos:");
+    loop {
+        // `read()` blocks until an `Event` is available
+        match read()? {
+            Event::Key(event) => {
+                match event.code {
+                    KeyCode::Esc => {break;}
+                    KeyCode::Down => {
+                        stdout().queue(MoveDown(1))?.flush()?;
+                    }
+                    KeyCode::Up => {
+                        stdout().queue(MoveUp(1))?.flush()?;
+                    }
+                    KeyCode::Left => {
+                        stdout().queue(MoveLeft(1))?.flush()?;
+                    }
+                    KeyCode::Right => {
+                        stdout().queue(MoveRight(1))?.flush()?;
+                    }
+                    KeyCode::Char(c) => {print!("{}", c); stdout().flush()?;}
+                    _ => {println!("{:?}\r", event.code)}
+                }
+            },
+            _ => {}
+
+        }
+    }
+    Ok(())
 }
